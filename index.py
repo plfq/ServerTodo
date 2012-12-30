2f74f2c19ec921b75f5fb83c7f9aa8573dc9be2f
Ma <562600828@qq.com>
2012-12-30 18:13:20 +0800

    开始正式编码了

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
 import web
+import os
+from setting import urls
 
-urls = ("/.*", "hello")
-app = web.application(urls, globals())
-
-class hello:
+if 'SERVER_SOFTWARE' in os.environ:
+#    Run in BAE
+    app = web.application(urls, globals()).wsgifunc()
+    from bae.core.wsgi import WSGIApplication
+    application = WSGIApplication(app)
+else:
+#    Run in Localhost
+    app = web.application(urls, globals())
+    if __name__ == "__main__":
+        app.run()
+class index:
+    name = "hello"
     def GET(self):
-        return 'Hello, world!'
+        return self.name
 
-if __name__ == "__main__":
-    app.run()
+
